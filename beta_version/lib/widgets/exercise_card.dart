622e25e3fcf82cba0a84af7bab29eafae6fe3a19
syleung <syleung1220@gmail.com>
2022-07-09 18:49:11 +0800

    exercise card completed

--- beta_version/lib/widgets/exercise_card.dart
+++ beta_version/lib/widgets/exercise_card.dart
@@ -1,8 +1,23 @@
 import 'package:custom_ui/custom_ui.dart';
 import 'package:flutter/material.dart';
 
-class exerciseCard_1 extends StatelessWidget {
-  exerciseCard_1({Key? key}) : super(key: key);
+class ExerciseCard1 extends StatelessWidget {
+  const ExerciseCard1({
+    Key? key,
+    required this.image,
+    required this.color,
+    required this.title,
+    required this.duration,
+    required this.category,
+    required this.description,
+  }) : super(key: key);
+
+  final String image;
+  final Color color;
+  final String title;
+  final String duration;
+  final String category;
+  final String description;
 
   @override
   Widget build(BuildContext context) {
@@ -10,11 +25,11 @@ class exerciseCard_1 extends StatelessWidget {
       width: double.infinity,
       height: 120,
       decoration: BoxDecoration(
-        color: AppColorsData.regular().orangeTints_4,
+        color: color, // AppColorsData.regular().orangeTints_4,
         image: DecorationImage(
-          image: AssetImage('assets/images/thinking.png'),
+          image: AssetImage(image), // 'assets/images/thinking.png'
           colorFilter: ColorFilter.mode(
-            AppColorsData.regular().orangeTints_4,
+            color, // AppColorsData.regular().orangeTints_4,
             BlendMode.hardLight,
           ),
           fit: BoxFit.fitHeight,
@@ -34,22 +49,94 @@ class exerciseCard_1 extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text("Cheek Strentheing",
+            Text(title, // "Cheek Strentheing",
                 style: AppTypographyData.primaryWhite().sourceSansProBodyBold),
             Row(
               children: <Widget>[
-                Text("4",
+                Text(duration, // "4",
                     style: AppTypographyData.greyShades_6()
                         .sourceSansProBodySmall),
                 Text(" mins * ",
                     style: AppTypographyData.greyShades_6()
                         .sourceSansProBodySmall),
-                Text("Cheek",
+                Text(category, // "Cheek",
                     style: AppTypographyData.greyShades_6()
                         .sourceSansProBodySmall),
               ],
             ),
-            Text("strengthe the muscles of the cheek",
+            Text(description, // "strengthe the muscles of the cheek",
+                style: AppTypographyData.greyShades_6().sourceSansProBodySmall),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DailyPracticeCard extends StatelessWidget {
+  const DailyPracticeCard({
+    Key? key,
+    required this.image,
+    required this.color,
+    required this.title,
+    required this.duration,
+    required this.category,
+    required this.description,
+  }) : super(key: key);
+
+  final String image;
+  final Color color;
+  final String title;
+  final String duration;
+  final String category;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 120,
+      decoration: BoxDecoration(
+        color: color, // AppColorsData.regular().orangeTints_4,
+        image: DecorationImage(
+          image: AssetImage(image), // 'assets/images/thinking.png'
+          colorFilter: ColorFilter.mode(
+            color, // AppColorsData.regular().orangeTints_4,
+            BlendMode.hardLight,
+          ),
+          fit: BoxFit.fitHeight,
+          alignment: Alignment.bottomRight,
+          repeat: ImageRepeat.noRepeat,
+        ),
+        borderRadius: const AppRadiusData.regular().asBorderRadius().allRegular,
+      ),
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.symmetric(horizontal: AppSpacingData.regular().x1),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 24.0,
+          horizontal: 16.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(title, // "Cheek Strentheing",
+                style: AppTypographyData.primaryWhite().sourceSansProBodyBold),
+            Row(
+              children: <Widget>[
+                Text(duration, // "4",
+                    style: AppTypographyData.greyShades_6()
+                        .sourceSansProBodySmall),
+                Text(" mins * ",
+                    style: AppTypographyData.greyShades_6()
+                        .sourceSansProBodySmall),
+                Text(category, // "Cheek",
+                    style: AppTypographyData.greyShades_6()
+                        .sourceSansProBodySmall),
+              ],
+            ),
+            Text(description, // "strengthe the muscles of the cheek",
                 style: AppTypographyData.greyShades_6().sourceSansProBodySmall),
           ],
         ),
